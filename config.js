bc2b55c9573589533105d6f8ab0fd1599af05f76
Jumpei Ogawa <phanect@users.noreply.github.com>
2019-03-25 15:27:54 +0900

    chore: replace rules with StandardJS

--- config.js
+++ config.js
@@ -3,6 +3,7 @@
 const js = {
         extends: [
           "eslint:recommended",
+          "standard",
           "plugin:security/recommended",
         ],
 
@@ -20,57 +21,57 @@ const js = {
           "security",
         ],
         rules: {
-          "comma-dangle": [ "error", "always-multiline" ],
-          "eol-last": [ "error", "always" ], // Not warning to keep diff in commit log readable
-          indent: [ "error", 2, {
-            SwitchCase: 1,
-            VariableDeclarator: { var: 2, let: 2, const: 3 },
-          }],
-          "no-trailing-spaces": "error", // Not warning to keep diff in commit log readable
-          "no-unused-expressions": "error",
-          "no-unused-labels": "error",
-          "no-unused-vars": "error",
-          "no-use-before-define": "error",
-          "no-whitespace-before-property": "error",
-          "no-var": "error",
-          "prefer-const": [ "error", {
-            destructuring: "all",
-            ignoreReadBeforeAssign: true,
-          }],
-          semi: [ "error", "always" ],
-          "unicode-bom": [ "error", "never" ],
-
+          // "comma-dangle": [ "error", "always-multiline" ],
+          // "eol-last": [ "error", "always" ], // Not warning to keep diff in commit log readable
+          // indent: [ "error", 2, {
+          //   SwitchCase: 1,
+          //   VariableDeclarator: { var: 2, let: 2, const: 3 },
+          // }],
+          // "no-trailing-spaces": "error", // Not warning to keep diff in commit log readable
+          // "no-unused-expressions": "error",
+          // "no-unused-labels": "error",
+          // "no-unused-vars": "error",
+          // "no-use-before-define": "error",
+          // "no-whitespace-before-property": "error",
+          // "no-var": "error",
+          // "prefer-const": [ "error", {
+          //   destructuring: "all",
+          //   ignoreReadBeforeAssign: true,
+          // }],
+          // semi: [ "error", "always" ],
+          // "unicode-bom": [ "error", "never" ],
           //
-          // Warnings
-          // These are just a preference in coding style.
-          // Following rules doesn't reduce quality or readability
+          // //
+          // // Warnings
+          // // These are just a preference in coding style.
+          // // Following rules doesn't reduce quality or readability
+          // //
+          // "array-bracket-spacing": [ "warn", "always", { arraysInArrays: false, objectsInArrays: false }],
+          // "brace-style": "warn",
+          // curly: "warn",
+          // "no-multi-spaces": [ "warn", { ignoreEOLComments: true, exceptions: { Property: true }}],
+          // "object-curly-spacing": [ "warn", "always", { arraysInObjects: false, objectsInObjects: false }],
+          // "one-var": [ "warn", {
+          //   const: "consecutive",
+          //   let: "consecutive",
+          //   separateRequires: true,
+          // }],
+          // "one-var-declaration-per-line": [ "warn", "initializations" ],
+          // "padded-blocks": [ "warn", "never" ],
+          // "prefer-arrow-callback": "warn",
+          // "quote-props": [ "warn", "as-needed" ],
+          // quotes: [ "warn", "double" ],
+          // "space-before-blocks": [ "warn", "always" ],
+          // "space-before-function-paren": [ "warn", {
+          //   anonymous: "never",
+          //   named: "never",
+          //   asyncArrow: "always",
+          // }],
+          // "space-in-parens": [ "warn", "never" ],
+          // "spaced-comment": [ "warn", "always" ],
+          // "switch-colon-spacing": [ "warn", { before: false, after: true }],
           //
-          "array-bracket-spacing": [ "warn", "always", { arraysInArrays: false, objectsInArrays: false }],
-          "brace-style": "warn",
-          curly: "warn",
-          "no-multi-spaces": [ "warn", { ignoreEOLComments: true, exceptions: { Property: true }}],
-          "object-curly-spacing": [ "warn", "always", { arraysInObjects: false, objectsInObjects: false }],
-          "one-var": [ "warn", {
-            const: "consecutive",
-            let: "consecutive",
-            separateRequires: true,
-          }],
-          "one-var-declaration-per-line": [ "warn", "initializations" ],
-          "padded-blocks": [ "warn", "never" ],
-          "prefer-arrow-callback": "warn",
-          "quote-props": [ "warn", "as-needed" ],
-          quotes: [ "warn", "double" ],
-          "space-before-blocks": [ "warn", "always" ],
-          "space-before-function-paren": [ "warn", {
-            anonymous: "never",
-            named: "never",
-            asyncArrow: "always",
-          }],
-          "space-in-parens": [ "warn", "never" ],
-          "spaced-comment": [ "warn", "always" ],
-          "switch-colon-spacing": [ "warn", { before: false, after: true }],
-
-          "no-console": "off",
+          // "no-console": "off",
         },
       },
       ts = {
